30654332d9ffcbb8e7fcddf21fac949e8acba7db
hellodigimate-ui <hello.digimate@gmail.com>
2026-03-28 17:48:38 +0530

    Add files via upload

--- apiHome/axiosInstanc.tsx
+++ apiHome/axiosInstanc.tsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "http://172.31.112.248:3000",
+  // baseURL: "http://192.168.158.248:8000",
   // baseURL: "http://192.168.1.25:8000",
+  // baseURL: "http://10.41.94.248:8000",
   baseURL: "https://api.schoolycore.com",
   // baseURL: "http://dezoryn-school.ap-south-1.elasticbeanstalk.com",
   headers: {
